3adfdd0fbd450b0144b31eb11d14224a3a0b8a4a
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 11:59:15 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.tugalsan.api.sql.max {
     requires java.sql;
     requires com.tugalsan.api.time;
     
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     requires com.tugalsan.api.tuple;
     
     requires com.tugalsan.api.log;
